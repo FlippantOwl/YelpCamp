b1a1c013a0a7e525bbd7f42269698b8d0c6e8e72
Mason Summers <mason-s@hotmail.co.uk>
2018-07-12 22:15:24 +0000

    correct mongoose connect

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes     = require("./routes/comments"),
     campgroundsRoutes = require("./routes/campgrounds"),    
     indexRoutes       = require("./routes/index");
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
+mongoose.connect("mongodb://Mason:testserv1@ds235711.mlab.com:35711/yelpcamp", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
